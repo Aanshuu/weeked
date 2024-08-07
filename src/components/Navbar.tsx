f8cf9e3a0473a67d9db51444424bb78e823f5c85
Aanshuu <e21cseu0085@bennett.edu.in>
2024-08-07 18:29:19 +0530

    Slight changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,9 +9,9 @@ const Navbar=()=>{
         <nav className='sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
             <MaxWidthWrapper>
                 <div className='flex h-14 items-center justify-between border-b border-zinc-200'>
-                <Link href='/' className='flex z-40 font-semibold'>
+                <TransitionLink href='/' className='flex z-40 font-semibold'>
                     <span>Weeked.</span>
-                </Link>
+                </TransitionLink>
 
                 {/* Mobile Navbar */}
 
